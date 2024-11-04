4278797757fe8c0d8cc80441437c712ae3c76c0e
isaiaha03 <isaiahalvarez2003@gmail.com>
2024-11-04 13:22:43 -0600

    Completed parts 3 and 4.

--- app.js
+++ app.js
@@ -42,3 +42,59 @@ function checkAge(userName, age)
 
 // Exercise 3 Section
 console.log("EXERCISE 3:\n==========\n");
+
+function whichQuadrant(x, y)
+{
+    if (x === 0 && y === 0)
+    {
+        console.log("The point (0, 0) is at the origin.");
+    }
+    else if (x === 0)
+    {
+        console.log(`The point (${x}, ${y}) lies on the y-axis.`);
+    }
+    else if (y === 0)
+    {
+        console.log(`The point (${x}, ${y}) lies on the x-axis.`);
+    }
+    else if (x > 0 && y > 0)
+    {
+        console.log(`The point (${x}, ${y}) lies in Quadrant 1.`);
+    }
+    else if (x < 0 && y > 0)
+    {
+        console.log(`The point (${x}, ${y}) lies in Quadrant 2.`);
+    }
+    else if (x < 0 && y < 0)
+    {
+        console.log(`The point (${x}, ${y}) lies in Quadrant 3.`);
+    }
+    else if (x > 0 && y < 0)
+    {
+        console.log(`The point (${x}, ${y}) lies in Quadrant 4.`);
+    }
+}
+
+// Exercise 4 Section
+console.log("EXERCISE 4:\n==========\n");
+
+function triangleType(a, b, c)
+{
+    if (a + b <= c || a + c <= b || b + c <= a)
+    {
+        return "Not a valid triangle: The sum of any two sides must be greater than the third."
+    }
+    if (a == b && b == c)
+    {
+        return "Equilateral triangle: All sides are equal.";
+    }
+    else if (a == b || a == c || b == c)
+    {
+        return "Isosceles triangle: Two sides are equal.";
+    }
+    else
+    {
+        return "Scalene triangle: All sides are different.";
+    }
+}
+
