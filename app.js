6f132d61e6be6a789224b8f3393f5baa34eebfbe
isaiaha03 <isaiahalvarez2003@gmail.com>
2024-11-04 13:11:40 -0600

    Completed parts 1 and 2.

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
-console.log("Hello World!\n==========\n");
+
+
+
+
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+function printOdds(count)
+{
+    if (count < 0)
+    {
+        console.log("Count isn't positive.");
+        return;
+    }
+    for (i = 0; i <= count; i++)
+    {
+        if(i % 2 == 1)
+        {
+            console.log(i);
+        }
+    }
+}
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+function checkAge(userName, age)
+{
+    const aboveSixteen = `Congrats ${userName}, you can drive!`;
+    const belowSixteen = `Sorry ${userName}, but you need to wait until you're 16.`;
+
+    if (age < 16)
+    {
+        console.log(belowSixteen);
+    }
+    else
+    {
+        console.log(aboveSixteen);
+    }
+}
+
+// Exercise 3 Section
+console.log("EXERCISE 3:\n==========\n");
